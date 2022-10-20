9b4b89e0df961e6f1a8413118a55df9dbdf01181
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-20 15:07:12 +0200

    to-be-fix-square

--- js/script.js
+++ js/script.js
@@ -4,3 +4,34 @@
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
+
+
+//variabili
+const container = document.querySelector('.container');
+const numCaselle = 100;
+
+
+//ciclo for per generare i quadratini
+for(let i = 0; i < numCaselle; i++){
+    squareGenerator(i);
+}
+
+
+
+
+
+
+
+// funzioni
+
+function squareGenerator(indexSquare){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.append(square);
+    square.style.width = genCssClass();
+    square.style.height = genCssClass();
+}
+
+function genCssClass(){
+    return `calc(100% / ${numCaselle})`;
+}
